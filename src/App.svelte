c2c0c84b9b4aea6206185aedff084331f42493d6
Joe Palala <jose.palala@live.com>
2022-07-01 02:08:33 +1000

    add the essential components!

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,97 @@
 <script>
   import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+	import NextPage from './NextPage.svelte';
+	import PreviousPage from './PreviousPage.svelte';
+	import { pageNum } from './stores.js';
+	
+  let page;
+	let pageNumber;
+	let imgWidth = 300;
+	let imgHeight = 200;
+	let slides = [
+    "<h1>welcome</h1>", 
+     "<p>I am this and that, I did this and that." , 
+     "<p>Put whatever here and style using global styles</p>", 
+     "<h3>End</h3>", 
+     "<h1>Thank you!</h1><a href=\"mailto:email-address@example.com\">email-address at example.com</a>"   ];
+
+	pageNum.subscribe(value => {
+			
+		pageNumber = value;
+		const content = slides[value - 1];
+		loadPage(content);
+	});
+	
+	function loadPage(content) {
+		if(content == undefined || content == null) {
+			//alert("cant load it sorry");
+		} else {
+      let ewanpage = document.querySelector(".display");
+			ewanpage.innerHTML = content;
+		}
+	}
 </script>
-
-<main>
+<div id="full_page">
+<div class="display"> innerHTML is the key.</div>
+<div class="footer">
+	<PreviousPage />
+<NextPage />
   <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
+	{pageNumber}
+	</div>
+</div>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+ :global(h1) {
+     font-size: 3rem;
+}
+ :global(p) {
+     font-size: 1.15rem;
+     font-weight: 600;
+}
+ :global(b) {
+     font-size: 1.5rem;
+     font-weight: 900;
+}
+ :global(img.s){
+     height: 52vh !important;
+     width: 80vw;
+     left: -15vw;
+     top: -5vh;
+     position: relative;
+}
+ :global(blockquote::before) {
+     font-weight: bold;
+     font-size: 1.3rem;
+     content: "\201C \A";
+}
+ :global(blockquote::after) {
+     font-weight: bold;
+     font-size: 1.3rem;
+     content: " \A \201D";
+}
+ #full_page {
+     position: absolute;
+     top: 0px;
+     left: 5px;
+     width: 100vw;
+     height: 100vh;
+     margin: 0.5rem;
+     padding: 0;
+     background: #111;
+     color: white !important;
+     display: flex;
+}
+/* this component inside can be the one that changes in svelte!! */
+ #full_page .display {
+     align-self: center;
+     margin-left: 30vw;
+}
+ .footer {
+     position:absolute;
+     bottom: 0px;
+     display: inline-block;
+}
 </style>
+
